e0341c68bd7fa7cf10d7bdcb1a1066903db43bdb
Myrteza <myrteza@hotmail.fr>
2021-10-28 12:51:40 +0200

    :style: (desktop) static style done

--- src/components/ProfileBox.js
+++ src/components/ProfileBox.js
@@ -3,14 +3,20 @@ import './ProfileBox.css';
 import ProfilePresentation from './ProfilePresentation'; 
 import StatsBox from './StatsBox'; 
 import Footer from './Footer'; 
+import ProfilePicture from './ProfilePicture'; 
 
 
 function ProfileBox(props) {
     return (
         <section className="ProfileBox">
-            <ProfilePresentation/>
-            <StatsBox/>
-            <Footer/>
+             <ProfilePicture    src={"https://i.pinimg.com/474x/11/d3/f2/11d3f2cb55ffbc652249f38d89fa2857--read-naruto-manga-anime-manga.jpg"}
+                                alt={"icon de Naruto"}/>
+
+            <div className="profileBoxMainCtn">
+                <ProfilePresentation/>
+                <StatsBox/>
+                <Footer/>
+            </div>
         </section>
     )
 }
--- src/components/ProfilePresentation.js
+++ src/components/ProfilePresentation.js
@@ -11,11 +11,7 @@ import Presentation from './Presentation';
 function ProfilePresentation(props) {
     return (
         <div className="ProfilePresentation">
-            <ProfilePicture src={"https://i.pinimg.com/474x/11/d3/f2/11d3f2cb55ffbc652249f38d89fa2857--read-naruto-manga-anime-manga.jpg"}
-                            alt={"icon de Naruto"}/>
-
                 <div className="profilPresentationMainCtn">
-
                     <div className="pseudoNameDateCtn">
                         <div className="pseudoNameCtn">
                             <Pseudo pseudo={"naruto"}/>
